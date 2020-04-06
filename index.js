0a3ffd788105b3ced3b534a0b039adca78320630
Sean Wines <seanwinesdev@gmail.com>
2020-04-06 10:17:26 -0700

    slider

---
+++ index.js
@@ -0,0 +1,58 @@
+var _slider = document.getElementById('sliderContainer'),
+    _sliderList = document.getElementById('sliderList'),
+    _prevSlide = document.getElementById('prevSlide'),
+    _nextSlide = document.getElementById('nextSlide'),
+    _slideLength = document.querySelectorAll('.quote-container').length;
+    _slides = document.querySelectorAll('.quote-container'),
+    _currentSlide = 0
+    slideListItems = [];
+
+    // set list dots
+    for (var i = 0; i < _slideLength; i++) {
+      var listItem = document.createElement('li');
+      listItem.setAttribute('idx', i)
+
+      // add click listener
+      listItem.addEventListener('click', function(e) {
+        changeSlide(e.target.getAttribute('idx'));
+      })
+      if (i == 0) listItem.classList += 'current';
+      _sliderList.appendChild(listItem)
+      slideListItems.push(listItem)
+    }
+
+    // handle arrows on click
+    _nextSlide.addEventListener('click', function() {
+      _currentSlide >= (_slideLength - 1) ? _currentSlide = 0 : _currentSlide++;
+      changeSlide()
+    })
+
+    prevSlide.addEventListener('click', function() {
+      console.log(_currentSlide)
+      console.log((_slideLength - 1))
+      console.log(_currentSlide <= (_slideLength - 1))
+      _currentSlide <= 0 ? _currentSlide = (_slideLength - 1) : _currentSlide--;
+      changeSlide()
+    })
+
+    function changeSlide(idx = _currentSlide) {
+      // remove selected class from slider dots
+      slideListItems.forEach(node => {
+        node.classList.remove(...node.classList);
+      })
+
+      // add new class to selected dot
+      slideListItems[idx].classList += 'current';
+
+      // hide prev slide
+      _slides.forEach(node => {
+        node.classList.remove('current-slide');
+        if (node.getAttribute('data-index') < idx) node.classList.add('hide-left');
+        if (node.getAttribute('data-index') > idx) node.classList.remove('hide-left');
+        if (node.getAttribute('data-index') == idx) {
+          node.classList.remove('hide-left');
+          node.classList.add('current-slide');
+        }
+      })
+
+    }
